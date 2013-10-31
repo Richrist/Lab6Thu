11b9f96115516bf65e5148b29751d60e3d0e5eb5
richrist <hockey_star100@hotmail.com>
2013-10-31 09:28:57 -0600

    Added new source file..Printtoscreen.java.  Counts from 0 - 15, outputting the number as it goes along

---
+++ src/clienteditor/Printtoscreen.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienteditor;
+
+/**
+ *
+ * @author richrist
+ */
+public class Printtoscreen {
+    public static void main(String [ ] args){
+        for(int i = 0; i < 15; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
